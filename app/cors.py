abd9238ee4709377c2183ff65655b6d37051bad9
kCn3333 <pawelkwiecienkrk@gmail.com>
2026-01-02 19:11:30 +0100

    fix: cors

--- app/cors.py
+++ app/cors.py
@@ -1,12 +1,16 @@
 import os
-from fastapi import Request, Response
+from fastapi import FastAPI, Request, Response
 
 ALLOWED_SUFFIX = os.getenv("ALLOWED_CORS_SUFFIX")
+app = FastAPI()
 
-def cors_middleware(request: Request, response: Response):
+@app.middleware("http")
+async def cors_middleware(request: Request, call_next):
+    response: Response = await call_next(request)
     origin = request.headers.get("origin")
     if origin and origin.endswith(ALLOWED_SUFFIX):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Vary"] = "Origin"
         response.headers["Access-Control-Allow-Methods"] = "GET"
         response.headers["Access-Control-Allow-Headers"] = "Accept"
+    return response
